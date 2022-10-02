6d15979b2022d456fdb01be64773e7045a0afa19
Marc Towler <marc@marctowler.co.uk>
2022-10-02 11:42:09 +0100

    update prod (#3)
    * 1.2.0 Update (#1) (#2)
    
    * Added Level 20 Monster
    
    * Added Config file
    Added Monster difficulty increases to non-boss levels
    Started to RNG Boss levels
    Bosses now RNG their colours
    
    * Added bosses:
    - Bat
    - Bug
    - Dino
    - Duggy
    - DustSpirit
    
    * Added:
    - RNG for boss monsters to appear
    - RNG on loot
    
    Fixed:
    - Bug where same loot appeared every chest
    - Missing bosses
    - Missing chests
    
    * Pushing v1.1 changes
    
    * V1.1.6 release
    
    Fixed:
    - Juice spawning too regularly
    - Skull key not being awarded on level 120
    - Stardrop not being awarded on level 100
    
    * v1.4.0 release. Added extra drops and increased reward based on difficulty

--- HardcoreMines2/ModConfig.cs
+++ HardcoreMines2/ModConfig.cs
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-﻿namespace HardcoreMines2
+namespace HardcoreMines2
 {
     internal class ModConfig
     {
         public double difficulty { get; set; } = 3;
-        public bool extraXP { get; set; } = false;
-    }
-}
-=======
-﻿namespace HardcoreMines2
-{
-    internal class ModConfig
-    {
-        public double difficulty { get; set; } = 3;
+        public double difficultyPct { get; set; } = -1; //-1 disables percentage and focuses on the 5 difficulty levels. Everything below 100% is a reduction, over 100% increases
         public bool extraXP { get; set; } = false;
         public bool skullCave { get; set; } = false;
         public bool volcanoDungeon { get; set; } = false;
     }
 }
->>>>>>> dev
--- HardcoreMines2/treasure_item.cs
+++ HardcoreMines2/treasure_item.cs
@@ -4,11 +4,13 @@
     {
         public int id;
         public int count;
+        public string type;
 
-        public treasure_item(int id, int count)
+        public treasure_item(int id, int count, string type = "")
         {
             this.id = id;
             this.count = count;
+            this.type = type;
         }
     }
 }
